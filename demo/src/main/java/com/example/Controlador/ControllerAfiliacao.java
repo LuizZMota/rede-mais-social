dd83b8ffc483418275b3fa9f170d13735c5e6e50
LuizZMota <luizzmota03@gmail.com>
2025-11-20 11:26:35 -0300

    aaa

--- demo/src/main/java/com/example/Controlador/ControllerAfiliacao.java
+++ demo/src/main/java/com/example/Controlador/ControllerAfiliacao.java
@@ -45,7 +45,7 @@ public class ControllerAfiliacao {
         }
         return false;
     }
-//ssssssssssssssssssssssss
+//aaaaaaaaa
     // Called if buscarCandidatoExistente is false
     public void iniciarNovoCandidato(String cpfOuCnpj, String email) {
         this.entidadeAtual = new Entidade();
